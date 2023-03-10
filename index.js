2ee5a92636e17a4487e36a20a0185e7a8649f9da
Octexor <octexor@pm.me>
2023-03-11 00:37:24 +0100

    prettify

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const crypto = require('crypto'),
-      envfile = require('envfile'),
-      writeFile = require('fs').writeFileSync,
-      readFile = require('fs').readFileSync;
-      exists = require('fs').existsSync;
+const crypto = require("crypto");
+const envfile = require("envfile");
+const { writeFile, readFile, exists } = require("fs");
 
-const ENCRYPTION_ALGORITHM = 'aes-256-ctr';
-const HMAC_ALGORITHM = 'sha256';
-const AUTHENTICATION_KEY = 'SENV_AUTHENTICATION';
-const AUTHENTICATION_SALT = 'SENV_SALT'
+const ENCRYPTION_ALGORITHM = "aes-256-ctr";
+const HMAC_ALGORITHM = "sha256";
+const AUTHENTICATION_KEY = "SENV_AUTHENTICATION";
+const AUTHENTICATION_SALT = "SENV_SALT";
 
-const PBKDF2_ITERATION_COUNT_FILE = 100000
-const PBKDF2_ITERATION_COUNT_STRING = 50000
+const PBKDF2_ITERATION_COUNT_FILE = 100000;
+const PBKDF2_ITERATION_COUNT_STRING = 50000;
 
 /**
  * Encrypts a string.
@@ -21,16 +19,22 @@ const PBKDF2_ITERATION_COUNT_STRING = 50000
  */
 async function encryptString(string, key, iv, name) {
     if (typeof key === "string") {
-        key = crypto.pbkdf2Sync(key, iv, PBKDF2_ITERATION_COUNT_STRING, 32, 'sha512');
+        key = crypto.pbkdf2Sync(
+            key,
+            iv,
+            PBKDF2_ITERATION_COUNT_STRING,
+            32,
+            "sha512"
+        );
     }
 
     if (name) {
-        iv = Buffer.from(createHmac(iv, name).slice(0, 32), 'hex')
+        iv = Buffer.from(createHmac(iv, name).slice(0, 32), "hex");
     }
 
     const cipher = crypto.createCipheriv(ENCRYPTION_ALGORITHM, key, iv);
-    let encrypted = cipher.update(string, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
+    let encrypted = cipher.update(string, "utf8", "hex");
+    encrypted += cipher.final("hex");
 
     return encrypted;
 }
@@ -44,16 +48,22 @@ async function encryptString(string, key, iv, name) {
  */
 async function decryptString(string, key, iv, name) {
     if (typeof key === "string") {
-        key = crypto.pbkdf2Sync(key, iv, PBKDF2_ITERATION_COUNT_STRING, 32, 'sha512');
+        key = crypto.pbkdf2Sync(
+            key,
+            iv,
+            PBKDF2_ITERATION_COUNT_STRING,
+            32,
+            "sha512"
+        );
     }
 
     if (name) {
-        iv = Buffer.from(createHmac(iv, name).slice(0, 32), 'hex')
+        iv = Buffer.from(createHmac(iv, name).slice(0, 32), "hex");
     }
 
     const decipher = crypto.createDecipheriv(ENCRYPTION_ALGORITHM, key, iv);
-    let decrypted = decipher.update(string, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
+    let decrypted = decipher.update(string, "hex", "utf8");
+    decrypted += decipher.final("utf8");
 
     return decrypted;
 }
@@ -62,13 +72,13 @@ async function decryptString(string, key, iv, name) {
  * Creates an HMAC from a string and password.
  * @param {string} string - The string to with which to create the HMAC.
  * @param {string} password - The password with which to create the HMAC.
- * 
+ *
  * @returns {string}  - The created HMAC.
  */
 function createHmac(string, password) {
     hmac = crypto.createHmac(HMAC_ALGORITHM, password);
     hmac.update(string);
-    return hmac.digest('hex');
+    return hmac.digest("hex");
 }
 
 /**
@@ -79,11 +89,11 @@ function createHmac(string, password) {
 function getPasswordFromEnvironment(fileName) {
     // Get password for individual .env file from environment variable
     const individualPasswordEnvVarName = fileName
-        .replace('.encrypted', '') // ignore encrypted filename part
-        .replace('.enc', '') // ignore encrypted filename part
-        .replace('.', 'DOT') // replace first . with DOT
-        .replace(/\./g, '_') // replace all other . with _
-        .concat('_PASS')
+        .replace(".encrypted", "") // ignore encrypted filename part
+        .replace(".enc", "") // ignore encrypted filename part
+        .replace(".", "DOT") // replace first . with DOT
+        .replace(/\./g, "_") // replace all other . with _
+        .concat("_PASS")
         .toUpperCase();
 
     if (process.env[individualPasswordEnvVarName]) {
@@ -92,32 +102,30 @@ function getPasswordFromEnvironment(fileName) {
 
     // Get password for individual .env file from password file
     const individualPasswordFileName = fileName
-        .replace('.encrypted', '') // ignore encrypted filename part
-        .replace('.enc', '') // ignore encrypted filename part
-        .concat('.pass');
+        .replace(".encrypted", "") // ignore encrypted filename part
+        .replace(".enc", "") // ignore encrypted filename part
+        .concat(".pass");
 
-    if(exists(individualPasswordFileName)) {
-        return readFile(individualPasswordFileName, 'utf8');
+    if (exists(individualPasswordFileName)) {
+        return readFile(individualPasswordFileName, "utf8");
     }
 
     // Get password for all .env files from environment variable
-    const globalPasswordEnvVarName = 'DOTENV_PASS';
+    const globalPasswordEnvVarName = "DOTENV_PASS";
 
     if (process.env[globalPasswordEnvVarName]) {
         return process.env[globalPasswordEnvVarName];
     }
 
     // Get password for all .env files from file
-    const globalPasswordFileName = '.env.pass';
+    const globalPasswordFileName = ".env.pass";
 
     if (exists(globalPasswordFileName)) {
-        return readFile(globalPasswordFileName, 'utf8');
+        return readFile(globalPasswordFileName, "utf8");
     }
 
     // if no password found, throw error
-    throw new Error(
-        'No password provided.'
-    );
+    throw new Error("No password provided.");
 }
 
 /**
@@ -125,40 +133,51 @@ function getPasswordFromEnvironment(fileName) {
  * @param {string} inputFile    - File path to plain text .env file to encrypt.
  * @param {string} outputFile   - File path to write encrypted .env file to.
  * @param {string} password     - The password with which to encrypt the .env file.
- * 
+ *
  * @return {string}            - If outputFile is undefined, encrypted .env contents will be
  *                               returned as a string. Otherwise returns success message.
  */
 async function encryptEnvFile(inputFile, outputFile, password) {
-    if(!password) {
+    if (!password) {
         password = getPasswordFromEnvironment(inputFile);
     }
 
     const envVariables = envfile.parseFileSync(inputFile);
 
-    const salt = crypto.randomBytes(16)
-    const key = crypto.pbkdf2Sync(password, salt, PBKDF2_ITERATION_COUNT_FILE, 32, 'sha512');
+    const salt = crypto.randomBytes(16);
+    const key = crypto.pbkdf2Sync(
+        password,
+        salt,
+        PBKDF2_ITERATION_COUNT_FILE,
+        32,
+        "sha512"
+    );
 
     const hmac = createHmac(JSON.stringify(envVariables), key);
 
     // 32 because hex. (16 bytes)
-    const iv = Buffer.from(hmac.slice(0, 32), 'hex');
+    const iv = Buffer.from(hmac.slice(0, 32), "hex");
 
     for (const variableName in envVariables) {
         if (envVariables.hasOwnProperty(variableName)) {
             const value = envVariables[variableName];
-            envVariables[variableName] = await encryptString(value, key, iv, variableName);
+            envVariables[variableName] = await encryptString(
+                value,
+                key,
+                iv,
+                variableName
+            );
         }
     }
 
     envVariables[AUTHENTICATION_KEY] = hmac;
-    envVariables[AUTHENTICATION_SALT] = salt.toString('hex');
+    envVariables[AUTHENTICATION_SALT] = salt.toString("hex");
 
     const encryptedEnvVariables = envfile.stringifySync(envVariables);
 
     if (outputFile) {
         writeFile(outputFile, encryptedEnvVariables);
-        return `Encrypted file successfully written to ${outputFile}`
+        return `Encrypted file successfully written to ${outputFile}`;
     } else {
         return encryptedEnvVariables;
     }
@@ -169,45 +188,56 @@ async function encryptEnvFile(inputFile, outputFile, password) {
  * @param {string} inputFile    - Path to encrypted .env file to decrypt.
  * @param {string} outputFile   - Path to write decrypted .env file to.
  * @param {string} password     - The password with which to decrypt the .env file.
- * 
+ *
  * @return {string}            - If outputFile is undefined, encrypted .env contents will be
  *                               returned as a string. Otherwise returns success message.
  */
 async function decryptEnvFile(inputFile, outputFile, password) {
-    if(!password) {
+    if (!password) {
         password = getPasswordFromEnvironment(inputFile);
     }
 
     const envVariables = envfile.parseFileSync(inputFile);
-    const salt = Buffer.from(envVariables[AUTHENTICATION_SALT], 'hex')
-    const key = crypto.pbkdf2Sync(password, salt, PBKDF2_ITERATION_COUNT_FILE, 32, 'sha512');
-    
+    const salt = Buffer.from(envVariables[AUTHENTICATION_SALT], "hex");
+    const key = crypto.pbkdf2Sync(
+        password,
+        salt,
+        PBKDF2_ITERATION_COUNT_FILE,
+        32,
+        "sha512"
+    );
+
     const hmac = envVariables[AUTHENTICATION_KEY];
 
     // 32 because hex. (16 bytes)
-    const iv = Buffer.from(hmac.slice(0, 32), 'hex');
+    const iv = Buffer.from(hmac.slice(0, 32), "hex");
 
-    delete envVariables[AUTHENTICATION_SALT]
+    delete envVariables[AUTHENTICATION_SALT];
     delete envVariables[AUTHENTICATION_KEY];
 
     for (const variableName in envVariables) {
         if (envVariables.hasOwnProperty(variableName)) {
             const encryptedValue = envVariables[variableName];
-            envVariables[variableName] = await decryptString(encryptedValue, key, iv, variableName);
+            envVariables[variableName] = await decryptString(
+                encryptedValue,
+                key,
+                iv,
+                variableName
+            );
         }
     }
 
     const calculatedHmac = createHmac(JSON.stringify(envVariables), key);
 
     if (hmac !== calculatedHmac) {
-        throw new Error('Incorrect password provided.');
+        throw new Error("Incorrect password provided.");
     }
 
     const decryptedEnvVariables = envfile.stringifySync(envVariables);
 
     if (outputFile) {
         writeFile(outputFile, decryptedEnvVariables);
-        return `Decrypted file successfully written to ${outputFile}`
+        return `Decrypted file successfully written to ${outputFile}`;
     } else {
         return decryptedEnvVariables;
     }
@@ -219,4 +249,4 @@ module.exports = {
     encryptString,
     decryptString,
     getPasswordFromEnvironment,
-}
+};
